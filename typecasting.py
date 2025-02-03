d1d75235ed54a6881f0fe662c48d26fa9b8bb9ae
rudra_gadila <rudragadila@gmail.com>
2025-02-02 20:00:51 -0500

    Typecasting

---
+++ typecasting.py
@@ -0,0 +1,14 @@
+x = 1
+y = 6.2
+z = True
+a = "Hello"
+print("Before Typecasting")
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+y = int(y)
+x = float(x)
+print("After Typecasting")
+print(type(x))
+print(type(y))
